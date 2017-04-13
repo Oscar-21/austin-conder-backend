85ba47b1041a8884abbc1cdd524d34ea264fb27d
theClubhouse <heythere@theclubhou.se>
2017-04-13 12:26:39 -0400

    got database for article up and running

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -11,7 +11,8 @@ class ArticlesController extends Controller
   // List of articles
   public function index()
   {
-
+    $articles = Article::all();
+    return Response::json($articles);
   }
 
   //store our Article
@@ -34,19 +35,31 @@ class ArticlesController extends Controller
   // update our article
   public function update($id, Request $request)
   {
-
+    $article = Article::find($id);
+    $article->title = $request->input('title');
+    $article->body = $request->input('body');
+    $image = $request->file('image');
+    $imageName = $image->getClientOriginalName();
+    $image->move("storage/", $imageName);
+    $article->image = $request->root()."/storage/".$imageName;
+    $article->save();
+    return Response::json(["success" => "Article Updated"]);
   }
 
   // show single article
     public function show($id)
   {
-
+    $article = Article::find($id);
+    return Response::json($article);
   }
 
     // delete a single article
     public function destroy($id)
   {
+    $article = Article::find($id);
+    $article->delete();
 
+    return Response::json(['success' => 'Deleted Article.']);
   }
 
 }
